a93efed03b52f068171b69eb2959b71c4b48ce7a
yanni1 <yanni.vandervoort@student.uantwerpen.be>
2025-04-25 10:12:05 +0200

    Working Pyhton version

--- ppp/reac_rate/__init__.py
+++ ppp/reac_rate/__init__.py
@@ -10,6 +10,5 @@ def reaction_rate(C_CO, C_CO2, C_O2):
     k_b = k0/4 * C_CO2
     k_f = k0 * C_CO * (C_O2 ** 2)
     k_consumption = k_f - k_b
-    print(type(k_consumption))
     return k_consumption
 
