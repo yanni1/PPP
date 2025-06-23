01f4e35bd03d0af6cad683e0a7a24b5086576f16
yanni1 <yanni.vandervoort@student.uantwerpen.be>
2025-06-23 15:32:51 +0200

    fixed profiler to work with flat vectors

--- ppp/Conc_Calc_CPP/Calc_CPP_Profiler.cpp
+++ ppp/Conc_Calc_CPP/Calc_CPP_Profiler.cpp
@@ -7,20 +7,28 @@
 
 int main(int, char**)
 {
-    int nt_given = 1000;
+    int nt_given = 1;
     params p = params(nt_given);
     cout << "nt set to: " << nt_given << '\n' << endl;
-    vector<vector<vector<vector<float>>>> Ct(nt_given, vector<vector<vector<float>>>(3, vector<vector<float>>(p.ny, vector<float>(p.nx, 0.0)))); // Ct will contain C_CO C_CO2 and C_O2 => C[time, species,[y,[x,conc_value]]]
-    vector<vector<vector<float> > > C(3, vector<vector<float> > (p.ny, vector<float>(p.nx, 0.0))); // C will contain C_CO C_CO2 and C_O2 => C[species,[y,[x,conc_value]]] 
-    vector<vector<vector<float> > > Cn(3, vector<vector<float> > (p.ny, vector<float>(p.nx, 0.0))); // output buffer => now only made once :)
-    fill(C[0][0].begin(), C[0][0].end(), p.CO_reservoir);
+    vector<float> Ct(nt_given * p.ns * p.ny * p.nx, 0.0f); //flattened vector values set to 0f (index = (s * ny + y) * nx + x)
+    vector<float> C(p.ns * p.ny * p.nx, 0.0f); //flattened input buffer
+    vector<float> Cn(p.ns * p.ny * p.nx, 0.0f); //flattened output buffer
+    fill(C.begin() + p.idx(0, 0, 0), C.begin() + p.idx(0, 0, 0) + p.nx, p.CO_reservoir);
 
     for (int n = 0; n < nt_given; n++) {
         cout << "timestep:" << n << '\n' << endl;
-        CC_CPP(p, C, Cn);  // evolve concentration directly in preallocated vectors, only passed as pointers => using 2 buffers essentially
-        Ct[n] = C;                  // store current timestep "snapshot" into Ct
-        swap(C, Cn); // reuse memory
-    }                
+        CC_CPP(p, C, Cn); 
+        for (int s = 0; s < p.ns; ++s) {
+            for (int y = 0; y < p.ny; ++y) {
+                for (int x = 0; x < p.nx; ++x) {
+                    int ct_idx = ((n * p.ns + s) * p.ny + y) * p.nx + x;
+                    int c_idx = (s * p.ny + y) * p.nx + x;
+                    Ct[ct_idx] = C[c_idx];
+                }
+            }
+        }
+        swap(C, Cn); // reuse buffers for next step without copying => C@n -> C@n+1 (input) and Cn now holds old state (C@n) => does not matter, gets replaced by computed data anyway               
+    }    
 
     return 0;
 }
