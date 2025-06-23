fc213b1756b5a2492709028649426c2635ccd880
yanni1 <yanni.vandervoort@student.uantwerpen.be>
2025-06-23 15:57:48 +0200

    deletion of unnused test/debug files

--- ppp/Conc_Calc_CPP/Reac_Rate_CPP.cpp
+++ ppp/Conc_Calc_CPP/Reac_Rate_CPP.cpp
@@ -3,9 +3,6 @@
 using namespace std;
 
 float reaction_rate_cpp(const vector<float> &C, int j, int i, const params& p){ 
-
-
-
     float k_b = (p.k0 * 0.25) * C[p.idx(1,j,i)];
     float k_f = (p.k0) * C[p.idx(0,j,i)] * C[p.idx(2,j,i)];
     float k_consumption = k_f - k_b;
