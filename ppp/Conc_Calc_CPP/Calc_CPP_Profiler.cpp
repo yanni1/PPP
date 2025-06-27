f88d607c44f840842e22ae6098d21bebba566c23
yanni1 <yanni.vandervoort@student.uantwerpen.be>
2025-06-27 13:11:18 +0200

    updated profiler to work with eps_field

--- ppp/Conc_Calc_CPP/Calc_CPP_Profiler.cpp
+++ ppp/Conc_Calc_CPP/Calc_CPP_Profiler.cpp
@@ -15,9 +15,21 @@ int main(int, char**)
     vector<float> Cn(p.ns * p.ny * p.nx, 0.0f); //flattened output buffer
     fill(C.begin() + p.idx(0, 0, 0), C.begin() + p.idx(0, 0, 0) + p.nx, p.CO_reservoir);
 
+    vector<float> eps_field(p.ny * p.nx, 0.0f);
+    vector<float> eps_series(nt_given * p.ny * p.nx, 0.0f);
+    int spatial_size = p.ny * p.nx;
+
      for (int n = 0; n < nt_given; n++) {
         cout << "timestep:" << n << '\n' << endl;
-        CC_CPP(p, C, Cn);
+        fill(eps_field.begin(), eps_field.end(), 0.0f);
+        CC_CPP(p, C, Cn, eps_field); 
+
+        int t_offset = n * spatial_size;
+        #pragma omp parallel for simd
+        for (int idx = 0; idx < spatial_size; idx++) {
+            eps_series[t_offset + idx] = eps_field[idx];
+        };
+        
         #pragma omp parallel for simd
         for (int idx = 0; idx < p.ns * p.ny * p.nx; idx++) {
             Ct[n * p.ns * p.ny * p.nx + idx] = C[idx];
